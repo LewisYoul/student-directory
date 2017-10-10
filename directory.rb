4a6cc05db98150331b1889b4886983671349f60d
Lewis <lewisyoul@gmail.com>
2017-10-10 15:57:13 +0100

    added request for cohort + info_getter method

--- directory.rb
+++ directory.rb
@@ -14,22 +14,47 @@ students = [
 ]
 =end
 
+
+
+
+
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
+  puts "Please enter the names and cohorts of the students"
+  puts "To finish, just hit return at name entry"
+
   students = []
-  #obtain input from the user
-  name = gets.chomp
-  #while name is NOT empty execute the following code
-  while !name.empty? do
-    students << {name: name, cohort: :november, hobby: :tennis, height: "6ft"}
-    puts "Now we have #{students.count} students"
-    #obtain another name from the user
-    name = gets.chomp
+  info = info_getter
+  #converted from .each iteration
+  while !info[0].empty? do
+    hash = Hash.new { |this_hash, key| this_hash[key] = :none }
+    hash[:name] = info[0]
+    if info[1].empty?
+      hash[:cohort]
+    else
+      hash[:cohort] = info[1]
+    end
+    students << hash
+    info = info_getter
   end
   #return the array of students
   students
+end
+
+def info_getter
+  output = []
+  go = ""
+  until go == "Y"
+    puts "Please enter a name"
+    output[0] = gets.chomp.to_sym
+    return output if output[0].empty?
+    puts "Please enter #{output[0]}'s cohort"
+    output[1] = gets.chomp.to_sym
+    puts "You entered Name: #{output[0]} and Cohort: #{output[1]}, is this correct?"
+    puts "If correct type Y, if you would like to enter again type N"
+    go = gets.chomp
+  end
+  output
 end
 
 def print_header
@@ -41,13 +66,17 @@ def print(students)
   count = 1
   until count == students.length+1
     current = students[count-1]
-    puts "#{count}. #{current[:name]} - #{current[:hobby]} - #{current[:height]}".center(100)
+    puts "#{count}. #{current[:name]} - #{current[:cohort]}".center(100)
     count += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall we have #{students.count} great students.".center(100)
+  if students.length == 1
+    puts "Overall we have #{students.count} great student.".center(100)
+  else
+    puts "Overall we have #{students.count} great students.".center(100)
+  end
 end
 
 students = input_students
